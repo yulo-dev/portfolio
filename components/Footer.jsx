e13664caff8629252d45f6aacbf355015c3f055a
Noelle Lo <noelleloyu@gmail.com>
2025-08-14 22:21:18 -0700

    update info

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = ({isDarkMode}) => {
 
         <div className='w-max flex items-center gap-2 mx-auto'>
             <Image src={isDarkMode ? assets.mail_icon_dark : assets.mail_icon} alt='' className='w-6'/>
-            yulo.jobs@gmail.com
+            loyu.jobs@gmail.com
         </div>
       </div>
 
